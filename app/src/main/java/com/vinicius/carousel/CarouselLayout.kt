ea37578f104dcc3c68a303c9257c2acd7e0c9bf9
KhomDrake <vinicius-de-viana@hotmail.com>
2022-09-09 23:47:57 -0300

    Adding more things to CarouselLayout.kt

--- app/src/main/java/com/vinicius/carousel/CarouselLayout.kt
+++ app/src/main/java/com/vinicius/carousel/CarouselLayout.kt
@@ -1,23 +1,154 @@
 package com.vinicius.carousel
 
 import android.content.Context
-import android.content.res.Resources
-import android.graphics.Rect
 import android.util.AttributeSet
 import android.util.Log
-import android.view.MotionEvent
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.HorizontalScrollView
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.view.children
+import androidx.core.view.isVisible
 import androidx.core.view.setPadding
+import androidx.recyclerview.widget.DiffUtil
+import androidx.transition.TransitionManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlin.math.abs
 
-abstract class CarouselAdapter<T>() {
+private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
-    private var list: MutableList<T> = mutableListOf()
+abstract class CarouselAdapter {
+
+    private lateinit var parent: ViewGroup
+    protected var onUpdate: (List<UpdateCarousel>) -> Unit = { }
+
+    abstract fun bind(position: Int, view: View)
+
+    abstract fun getItemCount(): Int
+
+    open fun getViewType(position: Int): Int = 0
+
+    abstract fun createView(viewType: Int, parent: ViewGroup): View
+
+    fun setParent(parent: ViewGroup) {
+        this.parent = parent
+    }
+
+    fun setOnUpdateItems(update: (List<UpdateCarousel>) -> Unit) {
+        onUpdate = update
+    }
 
 }
+
+class UpdateCarousel(
+    val position: Int,
+    val type: UpdateCarouselType,
+    val oldPosition: Int
+)
+
+enum class UpdateCarouselType {
+    MOVE,
+    REMOVE,
+    UPDATE,
+    ADD
+}
+
+abstract class ListCarouselAdapter<T : Any>(private val diffUtil: DiffUtil.ItemCallback<T>) : CarouselAdapter() {
+
+    protected val items: MutableList<T> = mutableListOf()
+
+    val currentList: List<T>
+        get() = items
+
+    abstract override fun bind(position: Int, view: View)
+
+    override fun getItemCount() = items.size
+
+    abstract override fun createView(viewType: Int, parent: ViewGroup): View
+
+    fun submitList(newList: List<T>) {
+        scope.launch {
+            val updates = mutableListOf<UpdateCarousel>()
+            val removedItems = items.filter { item ->
+                !newList.contains(item)
+            }.map { items.indexOf(it) }
+            newList.forEachIndexed { index, newItem ->
+                var isNewItem = true
+                var oldPosition = -1
+                var hasNewContent = false
+                items.forEachIndexed { indexOld, oldItem ->
+                    val areItemsTheSame = diffUtil.areItemsTheSame(oldItem, newItem)
+                    if(areItemsTheSame) {
+                        isNewItem = false
+                        val areContentsTheSame = diffUtil.areContentsTheSame(oldItem, newItem)
+                        hasNewContent = !areContentsTheSame
+                        if(indexOld != index) oldPosition = indexOld
+                        return@forEachIndexed
+                    }
+                }
+                if(isNewItem) {
+                    updates.add(
+                        UpdateCarousel(index, UpdateCarouselType.ADD, oldPosition)
+                    )
+                }
+
+                if(!isNewItem && oldPosition != -1) {
+                    updates.add(
+                        UpdateCarousel(index, UpdateCarouselType.MOVE, oldPosition)
+                    )
+                }
+
+                if(hasNewContent) {
+                    updates.add(
+                        UpdateCarousel(index, UpdateCarouselType.UPDATE, oldPosition)
+                    )
+                }
+            }
+            removedItems.forEach {
+                updates.add(UpdateCarousel(it, UpdateCarouselType.REMOVE, it))
+            }
+            items.clear()
+            items.addAll(newList)
+
+            launch(Dispatchers.Main) {
+                onUpdate.invoke(updates)
+            }
+        }
+    }
+
+}
+
+class MyAdapter: ListCarouselAdapter<MyObject>(MyDiffUtil()) {
+
+    override fun bind(position: Int, view: View) {
+        view.findViewById<AppCompatTextView>(R.id.title).text = items[position].name
+    }
+
+    override fun createView(viewType: Int, parent: ViewGroup): View {
+        return LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+    }
+
+}
+
+class MyObject(val id: Int, val name: String)
+
+class MyDiffUtil: DiffUtil.ItemCallback<MyObject>() {
+    override fun areItemsTheSame(oldItem: MyObject, newItem: MyObject): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: MyObject, newItem: MyObject): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+}
+
 
 class CarouselLayout @JvmOverloads constructor(
     context: Context,
@@ -26,6 +157,7 @@ class CarouselLayout @JvmOverloads constructor(
 ) : HorizontalScrollView(context, attrs, defStyleAttr) {
 
     private val root: LinearLayoutCompat
+    private var carouselAdapter: CarouselAdapter? = null
 
     init {
         root = LinearLayoutCompat(context).apply {
@@ -36,68 +168,106 @@ class CarouselLayout @JvmOverloads constructor(
             )
         }
         addView(root)
-        addTexts(root)
     }
 
-    private var moveTo = false
-
-    override fun onTouchEvent(ev: MotionEvent): Boolean {
-        if(ev.action == MotionEvent.ACTION_MOVE) {
-            moveTo = true
+    private fun onUpdateItems(updates: List<UpdateCarousel>) {
+        val movesAndRemoves =
+            updates.filter { it.type == UpdateCarouselType.REMOVE || it.type == UpdateCarouselType.MOVE }
+        val rest = updates.filter {
+            it.type != UpdateCarouselType.REMOVE
+                && it.type != UpdateCarouselType.MOVE
         }
+        TransitionManager.beginDelayedTransition(root)
 
-        if(ev.action == MotionEvent.ACTION_UP) {
-            if(moveTo) {
-                Log.i("Vini", "Devia mover pro meio")
-                val midPositionGlobal = Resources.getSystem().displayMetrics.widthPixels / 2
-//                (getChildAt(0) as? ViewGroup)?.apply {
-//                    children.iterator().forEach { child ->
-//                        if(child.isCompletelyVisible()) {
-//                            val globalVisibilityRectangle = Rect()
-//                            child.getGlobalVisibleRect(globalVisibilityRectangle)
-//                            val midPosition = globalVisibilityRectangle.left + (child.width)
-//                            val distance = midPositionGlobal - midPosition
-//                            scrollTo(distance, 0)
-//                        }
-//                    }
-//                }
+        if(movesAndRemoves.isNotEmpty()) {
+            val views = mutableListOf<View?>()
+            root.children.iterator().forEach {
+                views.add(it)
             }
-            moveTo = false
-        }
-
-        return super.onTouchEvent(ev)
-    }
-
-    private fun View.isCompletelyVisible(): Boolean {
-        if(!isShown || visibility != VISIBLE) return false
-
-        val globalVisibilityRectangle = Rect()
-        getGlobalVisibleRect(globalVisibilityRectangle)
-        val visibleWidth = globalVisibilityRectangle.right - globalVisibilityRectangle.left
-        val actualWidth = measuredWidth
-        return visibleWidth == actualWidth && isOnTheScreen()
-    }
-
-    private fun View.isOnTheScreen() : Boolean {
-        if(!isShown || visibility != VISIBLE) return false
-
-        val width = Resources.getSystem().displayMetrics.widthPixels
-        val height = Resources.getSystem().displayMetrics.heightPixels
-        val actualPosition = Rect()
-        getGlobalVisibleRect(actualPosition)
-        val screen = Rect(0, 0, width, height)
-        return actualPosition.intersect(screen)
-    }
-
-    private fun addTexts(viewGroup: ViewGroup) {
-
-        listOf("Test222222222222222", "Test222222222222222222", "Test32222222222222", "Test42222222222222", "Test52222222222").forEach {
-            viewGroup.addView(
-                TextView(context).apply {
-                    text = it
-                    setPadding(16)
+            root.removeAllViews()
+            val new = movesAndRemoves.map { update ->
+                val view = if(update.type == UpdateCarouselType.MOVE) {
+                    views[update.oldPosition]
+                } else {
+                    views[update.oldPosition]
                 }
-            )
+                Pair(update, view)
+            }
+
+            new.filter { it.first.type == UpdateCarouselType.REMOVE }.forEach {
+                if(views[it.first.oldPosition] == it.second) {
+                    views[it.first.oldPosition] = null
+                }
+            }
+
+            views.filterNotNull().takeIf { it.isNotEmpty() }?.let {
+                views.clear()
+                views.addAll(it)
+            }
+
+            new.filter { it.first.type == UpdateCarouselType.MOVE }.forEach {
+                views[it.first.position] = it.second
+            }
+
+            views.filterNotNull().forEach {
+                root.addView(it)
+            }
         }
+
+
+
+        rest.forEach { updateCarousel ->
+            val viewPosition = updateCarousel.position
+            val child: View? = root.getChildAt(viewPosition)
+            when(updateCarousel.type) {
+                UpdateCarouselType.UPDATE -> {
+                    carouselAdapter?.let {
+                        val viewType = it.getViewType(viewPosition)
+                        val view = child ?: run {
+                            val view = it.createView(viewType, root)
+                            addViewAtPosition(view, viewPosition)
+                            view
+                        }
+                        it.bind(viewPosition, view)
+                    }
+                }
+                UpdateCarouselType.MOVE -> {
+                    val oldChild = root.getChildAt(updateCarousel.oldPosition)
+
+                    oldChild?.let {
+                        root.removeView(it)
+                        addViewAtPosition(it, updateCarousel.position)
+                    }
+                }
+                UpdateCarouselType.ADD -> {
+                    carouselAdapter?.let {
+                        val viewType = it.getViewType(viewPosition)
+                        val view = it.createView(viewType, root)
+                        addViewAtPosition(view, viewPosition)
+                        it.bind(viewPosition, view)
+                    }
+                }
+                else -> Unit
+            }
+        }
+    }
+
+    private fun addViewAtPosition(view: View, position: Int) {
+        if(root.childCount - 1 >= position) {
+            root.addView(view, position)
+        } else root.addView(view)
+    }
+
+    fun setCarouselAdapter(new: CarouselAdapter?) {
+        carouselAdapter = new
+        carouselAdapter?.setParent(root)
+        carouselAdapter?.setOnUpdateItems(::onUpdateItems)
+        if(carouselAdapter == null) {
+            clear()
+        }
+    }
+
+    fun clear() {
+        root.removeAllViews()
     }
 }
--- app/src/main/java/com/vinicius/carousel/MainActivity.kt
+++ app/src/main/java/com/vinicius/carousel/MainActivity.kt
@@ -2,11 +2,46 @@ package com.vinicius.carousel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val adapter = MyAdapter()
+        val add = findViewById<AppCompatTextView>(R.id.add)
+        val remove = findViewById<AppCompatTextView>(R.id.remove)
+        val move = findViewById<AppCompatTextView>(R.id.move)
+        add.setOnClickListener {
+            val currentList = adapter.currentList.toMutableList()
+            currentList.add(MyObject(Random.nextInt(0, Int.MAX_VALUE), "W ${Random.nextInt(0, 1000)}    "))
+            adapter.submitList(currentList)
+        }
+
+        remove.setOnClickListener {
+            val currentList = adapter.currentList.toMutableList()
+            val randomIndex = Random.nextInt(0, currentList.size)
+            currentList.removeAt(randomIndex)
+            adapter.submitList(currentList)
+        }
+
+        move.setOnClickListener {
+            val currentList = adapter.currentList.toMutableList()
+            val randomIndex = Random.nextInt(0, currentList.size)
+            val item = currentList[randomIndex]
+            val randomIndex2 = Random.nextInt(0, currentList.size)
+
+            Log.i("Vini", "${item.name} OldIndex: $randomIndex, newIndex: $randomIndex2")
+            currentList.remove(item)
+            currentList.add(randomIndex2, item)
+            adapter.submitList(currentList)
+        }
+
+        findViewById<CarouselLayout>(R.id.carousel).apply {
+            setCarouselAdapter(adapter)
+        }
     }
 }
